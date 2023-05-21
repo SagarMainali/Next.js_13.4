800d11deede91d12ea4446d60952a32aba21c5e3
Sagar Mainali <sagarmainali78@gmail.com>
2023-05-21 22:36:00 +0545

    added footer

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,9 @@
+import '@/app/style.css'
+
+export default function Footer() {
+     return (
+          <footer>
+               Reddit Inc. Protected by &copy;Copyright 2023.
+          </footer>
+     )
+}
